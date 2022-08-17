414421f8355a6d699cab121e0a3882cbceac8eec
Neha-mtalkz <neha.yadav@mtalkz.com>
2022-08-17 15:38:01 +0530

    add changes

--- JobQueues/Consumer/orders-queue-consumer.js
+++ JobQueues/Consumer/orders-queue-consumer.js
@@ -1,6 +1,6 @@
 const {msgModel} = require('./db-connection/connection');
 let count = 0
-
+console.log("KKKKKKKKKK");
 const ordersProcess = async (job) => {
     let result = await msgModel.create(job.data);
     count = count + 1;
